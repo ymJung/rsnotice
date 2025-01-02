1aa15b91bfe3f25af596100651400dd75f0714a1
ymJung <metalbird0@gmail.com>
2025-01-02 22:51:37 +0900

    initialize

---
+++ src/main/java/com/rsnotice/rsnotice/RsnoticeApplication.java
@@ -0,0 +1,13 @@
+package com.rsnotice.rsnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsnoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsnoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsnotice/rsnotice/RsnoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsnotice.rsnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsnoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
